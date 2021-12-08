0ea26b9ef4980b49a9df25334c0b73129692bf92
An <anknguyenanknguyen@gmail.com>
2021-12-07 21:09:59 -0800

    :speech_balloon: add last update date to footer
    add to beginning a footer a line stating when the last update date of the stats was

--- components/Footer.js
+++ components/Footer.js
@@ -8,24 +8,28 @@ export default function Footer() {
     return (
         <div className={styles.footer}>
             <hr className={styles.hr} />
-            This site was created by{" "}
-            <a href="https://twitter.com/nguyenank_">An Nguyen</a> using
-            Next.js, based on the{" "}
-            <a href="https://www.theicegarden.com/2021/11/4/22760382/the-ice-garden-fantasy-hockey-nwhl-phf-2021-22-womens-hockey">
-                PHF 2021-2022 Fantasy League
-            </a>{" "}
-            run by <a href="https://www.theicegarden.com/">The Ice Garden</a>.
-            Data is taken from a{" "}
-            <a href="https://docs.google.com/spreadsheets/d/15kkJUbxm7UAyq57Ujxv9BORk2vb06M6Ck1bL7XkPC30/edit?usp=sharing">
-                spreadsheet of counting stats
-            </a>{" "}
-            collected by{" "}
-            <a href="https://twitter.com/DigDeepBSB">Mike Murphy</a>. The source
-            code for the application is available at{" "}
-            <a href="https://github.com/nguyenank/fantasy-hockey-league">
-                GitHub
-            </a>
-            .
+            <p>Last updated December 7, 2021.</p>
+            <p>
+                This site was created by{" "}
+                <a href="https://twitter.com/nguyenank_">An Nguyen</a> using
+                Next.js, based on the{" "}
+                <a href="https://www.theicegarden.com/2021/11/4/22760382/the-ice-garden-fantasy-hockey-nwhl-phf-2021-22-womens-hockey">
+                    PHF 2021-2022 Fantasy League
+                </a>{" "}
+                run by{" "}
+                <a href="https://www.theicegarden.com/">The Ice Garden</a>. Data
+                is taken from a{" "}
+                <a href="https://docs.google.com/spreadsheets/d/15kkJUbxm7UAyq57Ujxv9BORk2vb06M6Ck1bL7XkPC30/edit?usp=sharing">
+                    spreadsheet of counting stats
+                </a>{" "}
+                collected by{" "}
+                <a href="https://twitter.com/DigDeepBSB">Mike Murphy</a>. The
+                source code for the application is available at{" "}
+                <a href="https://github.com/nguyenank/fantasy-hockey-league">
+                    GitHub
+                </a>
+                .
+            </p>
         </div>
     );
 }
